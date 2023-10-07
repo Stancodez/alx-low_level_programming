bd6aa2702fcea13ab80a9f8bb7b4d0adc8b868fa
Stanley <Stanleyemeke71@gmail.com>
2023-10-07 01:51:18 -0700

    hello

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,12 +3,12 @@
 /**
  * main - this is the main function
  *
- * Return:  always 0
+ * Return: always 0
  */
 
 
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle);
-	retun (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
