e3cda882566ed486a1c11e94c0526a3592fbe3b1
Stanley <Stanleyemeke71@gmail.com>
2023-10-10 18:30:36 -0700

    sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -7,7 +7,7 @@
  */
 
 int print_sign(int n)
-{
+	{
 	if (n > 0)
 	{_putchar('+');
 	return (1);
@@ -18,7 +18,7 @@ int print_sign(int n)
 	return (0);
 	}
 	else
-	{	
+	{
 	_putchar('-');
 	return (-1);
 	}
