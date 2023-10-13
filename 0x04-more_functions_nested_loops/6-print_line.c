b5883e7d34e4fd8cf76c59e315fa4fea6e39350c
Stanley <Stanleyemeke71@gmail.com>
2023-10-13 03:49:42 -0700

    task 6

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -7,16 +7,13 @@
 void print_line(int n)
 {
 	if (n <= 0)
-	{
 		_putchar('\n');
-	} else
+	else
 	{
 		int i;
 
 		for (i = 1 ; i <= n ; i++)
-		{
-			putchar('_');
-		}
+			_putchar('_');
 		_putchar('\n');
 	}
 }
